6ee48593c4756c7c4a9a4a2511b533a01bfa27f6
Your Name <you@example.com>
2019-12-06 15:58:42 -0800

    reworking the code for main in week04-docker-compose

---
+++ week04-docker-composer/system-environment/routes/exec-git.js
@@ -0,0 +1,34 @@
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+
+const workingDir = 'week04-docker-composer';
+
+let allBranches = [];
+
+async function getBranches(res) {
+    const { stdout, stderr } = await exec('git branch -a | sed -n -e \'s/remotes.origin*.//p\' | grep -v \'HEAD\'', {
+        cwd: workingDir
+    });
+
+    const output = stdout.trim();
+    allBranches = output.split('\n').map(item => item.trim());
+    console.log('stdout:', output);
+    console.error('stderr:', stderr);
+    if (res) {
+        res.send({
+            result: 'success',
+            response: output,
+            branches: allBranches
+        });
+    } else {
+        return allBranches;
+    }
+}
+
+async function getNewRepo(res) {
+    response.send({result: "success"});
+}
+
+module.exports.getNewRepo = getNewRepo;
+module.exports.getBranches = getBranches;
+module.exports.workingDir = workingDir;
--- week04-docker-composer/system-environment/routes/index.js
+++ week04-docker-composer/system-environment/routes/index.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const { workingDir, getBranches, getNewRepo } = require('./exec-git');
 
 /* GET home page. */
 router.get('/', function(req, res) {
@@ -17,9 +18,23 @@ router.get('/you-rang', function(req, res){
         "server": "system-environment",
         "directory": __dirname,
         "hostname": process.env.HOSTNAME,
-        "home": process.env.HOME
+        "home": process.env.HOME,
+        "workingDir": workingDir
     };
     res.send(rangData);
+});
+
+router.get('/getBranches',function(req, res){
+    debug('Get Branches called');
+    getBranches(res).catch(function(e){
+        console.log(e);
+    })
+});
+
+router.get('/getNewRepo',function(req, res){
+    getNewRepo(res).catch(function(e){
+        console.log(e);
+    })
 });
 
 module.exports = router;
